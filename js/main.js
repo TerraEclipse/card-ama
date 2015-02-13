5ccbf640b46300e2fe07eadd79b5ddb1e193f6d3
Nick Badagliacca <nick@terraeclipse.com>
2015-02-13 09:05:13 -0800

    Removes conditional font shrinking to try new method

--- js/main.js
+++ js/main.js
@@ -20,12 +20,12 @@ $(function() {
     chars = letters+(spaces/2);
     rawSize = Math.round(((-0.00103*(Math.pow(chars, 2)))+(0.01765*chars)+48.6));
 
-    if (variables.variant == 'patient_risk' || variables.variant == 'patient_care_access') {
-      finalSize = Math.floor(rawSize * 0.8);
-    }
-    else {
-      finalSize = rawSize;
-    }
+    // if (variables.variant == 'patient_risk' || variables.variant == 'patient_care_access') {
+    //   finalSize = Math.floor(rawSize * 0.8);
+    // }
+    // else {
+    //   finalSize = rawSize;
+    // }
 
     $(selector).css({ 'font-size': finalSize + 'px' }); //
   }
