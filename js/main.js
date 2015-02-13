0a635d498acb23b6f041859d306fe40bf0b99f34
Nick Badagliacca <nick@terraeclipse.com>
2015-02-13 09:33:27 -0800

    Lower

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(function() {
           'physician_payment_cuts': 1.3,
           'physician_high_numbers': 1.5,
           'physician_investing': 1.3,
-          'physician_thank_you_a': 1.2,
+          'physician_thank_you_a': 1.1,
           'physician_thank_you_b': 1,
           'patient_care_availability': 1,
           'patient_care_access': 1,
