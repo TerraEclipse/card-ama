7accc02855965def8f7bc7ae2ff09a90d2ada21e
Nick Badagliacca <nick@terraeclipse.com>
2015-02-12 16:07:06 -0800

    Testing font size adjustments on 1/3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
   
-  function setFontSize(input, selector) {
+  function setFontSize(input, selector, loopNumber) {
     var charArray = input.split('')
       , letters = 0
       , spaces = 0
@@ -19,19 +19,23 @@ $(function() {
     chars = letters+(spaces/2);
     size = Math.round(((-0.00103*(Math.pow(chars, 2)))+(0.01765*chars)+48.6));
 
+    if (loopNumber == 1 || loopNumber == 3) {
+      $(selector).css({ color: 'red' });
+    }
+
     $(selector).css({ 'font-size': size + 'px' });
   }
 
   function setLoopNumber(variantArray, selector) {
-    var loopNumber = variantArray.length + 1;
+    var text = document.getElementsByTagName('h1')[0].innerText
+      , loopNumber = variantArray.length + 1;
 
     $(selector).addClass('loop-' + loopNumber);
+
+    setFontSize(text, 'h1', loopNumber);
   }
 
   $(document).ready(function() {
-    var text = document.getElementsByTagName('h1')[0].innerText;
-
-    setFontSize(text, 'h1');
     setLoopNumber(variables.variants_shared, 'body');
   });
 
