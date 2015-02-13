478b88b1aaafd6ea7f444eb97e878ed6a5281024
Nick Badagliacca <nick@terraeclipse.com>
2015-02-13 13:04:09 -0800

    Wrong variant

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ $(function() {
       , finalSize = 0
       , manualAdjustments = {
           'physician_payment_cuts': 1.3,
-          'physician_high_numbers': 1.5,
-          'physician_investing': 1.1,
+          'physician_high_numbers': 1.3,
+          'physician_investing': 1.3,
           'physician_thank_you_a': 1.15,
           'physician_thank_you_b': 1,
           'patient_care_availability': 1.15,
