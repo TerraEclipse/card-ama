f9427b820d7acfb59376e8843d2cfbb7dd63cbe7
Nick Badagliacca <nick@terraeclipse.com>
2015-02-16 12:49:05 -0800

    Changed again

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(function() {
       , manualAdjustments = {
           'physician_payment_cuts': 1,
           'physician_high_numbers': 1.1,
-          'physician_investing': 1,
+          'physician_investing': 0.8,
           'physician_thank_you_a': 1,
           'physician_thank_you_b': 1,
           'patient_care_availability': 1.15,
